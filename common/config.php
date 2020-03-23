4d578a00d5b209a4d28b92880fc2cd01680a154a
Carlos <carlosguillermo@outlook.com>
2020-03-23 23:00:59 +0100

    Implementa la configuración de la conexión a la base de datos

---
+++ common/config.php
@@ -0,0 +1,11 @@
+<?php
+    $config = array(
+        'database' => array(
+            'host' => 'localhost',
+            'username' => 'usr',
+            'password' => 'mysql',
+            'database' => 'gallery'
+        ),
+
+    );
+?>
