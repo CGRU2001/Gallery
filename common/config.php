e6a93170879324eff398291f5615de6888e7629d
Carlos <carlosguillermoru@gmail.com>
2020-03-29 20:31:25 +0200

    Implementa el resto del proyecto (forgot to do commit)

--- common/config.php
+++ common/config.php
@@ -1,10 +1,11 @@
 <?php
     $config = array(
         'database' => array(
-            'host' => 'localhost',
-            'username' => 'usr',
-            'password' => 'mysql',
-            'database' => 'gallery'
+            'host' => '192.168.1.153',
+            'username' => 'carlos',
+            'password' => 'Carlos10032001',
+            'database' => 'gallery',
+            'encoding' => 'utf-8'
         ),
 
     );
